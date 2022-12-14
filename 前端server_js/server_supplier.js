b2c63117405c7796c6a7b97b4789d9625fe90d95
dafa1122 <119932185+dafa1122@users.noreply.github.com>
2022-12-14 21:58:50 +0800

    Add files via upload
    js

---
+++ 前端server_js/server_supplier.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser');
+
+const app = express();
+// 启动gzip
+app.use(compression());
+
+// 在express中使用的时候需要注意一点的是，该中间件必须要放在express.static中间前的前面引入，否则会出现问题。
+app.use(history());
+app.use(bodyParser.json({
+    limit: '10MB'
+}));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(cookieParser());
+
+// app.use(express.static('dist'));
+
+//app.use(express.static('ruoyi-manage'));
+ app.use(express.static('dist'));
+app.listen(3001)
